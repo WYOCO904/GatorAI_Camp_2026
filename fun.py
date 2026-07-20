7d9994f5e8776f77e45d3a4619668085cf32697e
WYOCO904 <nicoellischan@gmail.com>
2026-07-20 14:52:26 -0400

    made code that gives name and age and asks for name and age and writes it our in days aswell

---
+++ fun.py
@@ -0,0 +1,7 @@
+name = "nicolas"
+age = 16
+age_days = age * 365
+print(f"{name} is {age} years old or {age_days} days old.")
+x = int(input("How old are you? "))
+y = str(input("What is your name?"))
+print(y, "is", x, "years old or", x * 365, "days old.")
