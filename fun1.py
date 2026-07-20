699be46cb19dda54baaf868c316d2e91645481c3
WYOCO904 <nicoellischan@gmail.com>
2026-07-20 14:52:03 -0400

    Made a code that asks for birthday and give age in days

---
+++ fun1.py
@@ -0,0 +1,16 @@
+from datetime import datetime, date
+
+birthday_input = input("Enter your birthday (YYYY-MM-DD): ")
+
+try:
+    birthday = datetime.strptime(birthday_input, "%Y-%m-%d").date()
+except ValueError:
+    print("Invalid date format. Please use YYYY-MM-DD.")
+else:
+    today = date.today()
+
+    if birthday > today:
+        print("Birthday cannot be in the future.")
+    else:
+        age_in_days = (today - birthday).days
+        print(f"You are {age_in_days} days old.")
